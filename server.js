b91f1782be8a8df6badf83d0674eb911fb20b44e
NzM <nazimfilzer@gmail.com>
2023-11-11 12:05:55 +0530

    Merge CONFLICT

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const { sendMsg } = require("./src/services/whatsappService");
+const axios = require('axios');
+const cloudinary = require('cloudinary').v2;
+const { getMediaUrl, downloadAndUploadImage } = require('./src/utils/getImage');
+
+
 const app = express();
 app.use(bodyParser.json());
 const { sendMsg,sendMessageTemplate } = require("./src/services/whatsappService");
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 
 app.get("/", (req, res) => {
-  
   res.send("Medication Reminder Service is running.");
-
 });
 sendMsg("haistart",process.env.PHNO);
 sendMessageTemplate(process.env.PHNO);
 
+app.post("/sendMsg", (req, res) => {
+  const { message, number } = req.body;
+  sendMsg(message, number);
+  res.send("Message Sent");
+});
 
 let mytoken = "testing";
-//connecting with whatsapp
+
 app.get("/webhook", (req, res) => {
   let mode = req.query["hub.mode"];
   let challange = req.query["hub.challenge"];
@@ -34,6 +39,41 @@ app.get("/webhook", (req, res) => {
     }
   }
 });
-console.log("hfff");
 
 
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+});
+
+// Webhook endpoint to handle incoming messages
+app.post('/webhook', async (req, res) => {
+  const messages = req.body.entry[0].changes[0].value.messages;
+  if (messages && messages[0].type === 'image') {
+    const mediaId = messages[0].image.id;
+    const accessToken = process.env.WHATSAPP_API_TOKEN;
+    try {
+      const mediaUrl = await getMediaUrl(mediaId, accessToken);
+      const cloudinaryUrl = await downloadAndUploadImage(mediaUrl, accessToken);
+      console.log('Cloudinary URL:', cloudinaryUrl);
+
+
+      res.sendStatus(200);
+    } catch (error) {
+      console.error('Error processing the image:', error);
+      res.sendStatus(500);
+    }
+  } else {
+    res.sendStatus(200);
+  }
+});
+
+sendMsg("Hello Peeps", +917907295072);
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ src/utils/getImage.js
@@ -0,0 +1,44 @@
+const cloudinary = require('cloudinary').v2;
+const axios = require('axios');
+
+cloudinary.config({
+    cloud_name: 'dvlfsldbh',
+    api_key: '746478836122374',
+    api_secret: 'm52kyprJ5e9iYpLiDMLCEk98wsI'
+});
+
+
+async function getMediaUrl(mediaId, accessToken) {
+    try {
+        const response = await axios.get(`https://graph.facebook.com/v18.0/${mediaId}/`, {
+            headers: { 'Authorization': `Bearer ${accessToken}` }
+        });
+        console.log(response.data);
+        return response.data.url;
+    } catch (error) {
+        console.error('Error retrieving media URL:', error);
+        throw error;
+    }
+}
+
+// Function to download and upload the image
+async function downloadAndUploadImage(mediaUrl, accessToken) {
+    try {
+        const mediaResp = await axios.get(mediaUrl, {
+            responseType: 'arraybuffer',
+            headers: { 'Authorization': `Bearer ${accessToken}` },
+        });
+
+        const uploadResp = await cloudinary.uploader.upload(`data:image/jpeg;base64,${Buffer.from(mediaResp.data).toString('base64')}`);
+        // console.log('Image uploaded to Cloudinary:', uploadResp.url);
+        return uploadResp.url;
+    } catch (error) {
+        console.error('Error in downloading/uploading image:', error);
+        throw error;
+    }
+}
+
+module.exports = {
+    getMediaUrl,
+    downloadAndUploadImage
+}
