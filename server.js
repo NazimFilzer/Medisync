01fd59968d5e1e965b0352add14c43f3819f6bed
EmmanuelSibi <emmanuelsiby2003@gmail.com>
2023-11-11 12:11:53 +0530

    interactive msg

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const { sendMsg,sendMessageTemplate } = require("./src/services/whatsappService");
-const axios = require('axios');
-const cloudinary = require('cloudinary').v2;
-const { getMediaUrl, downloadAndUploadImage } = require('./src/utils/getImage');
-
+const {
+  sendMsg,
+  sendMessageTemplate,
+} = require("./src/services/whatsappService");
+const axios = require("axios");
+const cloudinary = require("cloudinary").v2;
+const { getMediaUrl, downloadAndUploadImage } = require("./src/utils/getImage");
 
 const app = express();
 app.use(bodyParser.json());
-
 
 app.get("/", (req, res) => {
   res.send("Medication Reminder Service is running.");
@@ -39,29 +40,42 @@ app.get("/webhook", (req, res) => {
   }
 });
 
-
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 // Webhook endpoint to handle incoming messages
-app.post('/webhook', async (req, res) => {
+app.post("/webhook", async (req, res) => {
   const messages = req.body.entry[0].changes[0].value.messages;
-  if (messages && messages[0].type === 'image') {
+  if (messages && messages[0].type === "image") {
     const mediaId = messages[0].image.id;
     const accessToken = process.env.WHATSAPP_API_TOKEN;
     try {
       const mediaUrl = await getMediaUrl(mediaId, accessToken);
       const cloudinaryUrl = await downloadAndUploadImage(mediaUrl, accessToken);
-      console.log('Cloudinary URL:', cloudinaryUrl);
-
+      console.log("Cloudinary URL:", cloudinaryUrl);
 
       res.sendStatus(200);
     } catch (error) {
-      console.error('Error processing the image:', error);
+      console.error("Error processing the image:", error);
       res.sendStatus(500);
+    }
+  } else if (message.type === "interactive") {
+    const interactiveData = message.interactive;
+    if (interactiveData.type === "button_reply") {
+      const payload = interactiveData.button_reply.payload;
+
+      if (payload === "YES_BUTTON") {
+        console.log("User clicked YES");
+        sendMsg("you clicked yes", process.env.PHNO);
+        // Add your logic here for YES button click
+      } else if (payload === "NO_BUTTON") {
+        console.log("User clicked NO");
+        sendMsg("you clicked no", process.env.PHNO);
+        // Add your logic here for NO button click
+      }
     }
   } else {
     res.sendStatus(200);
@@ -69,8 +83,6 @@ app.post('/webhook', async (req, res) => {
 });
 
 sendMsg("Hello Peeps", process.env.PHNO);
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
