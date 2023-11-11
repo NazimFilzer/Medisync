9155e186e82de942d2f4c178de99fe10bf09692b
EmmanuelSibi <emmanuelsiby2003@gmail.com>
2023-11-11 11:15:26 +0530

    initial install and whatspp conn

--- server.js
+++ server.js
@@ -14,3 +14,21 @@ app.get("/", (req, res) => {
   res.send("Medication Reminder Service is running.");
 
 });
+
+
+let mytoken = "testing";
+//connecting with whatsapp
+app.get("/webhook", (req, res) => {
+  let mode = req.query["hub.mode"];
+  let challange = req.query["hub.challenge"];
+  let token = req.query["hub.verify_token"];
+
+  if (mode && token) {
+    if (mode === "subscribe" && token === mytoken) {
+      res.status(200).send(challange);
+    } else {
+      res.status(403);
+    }
+  }
+});
+console.log("hfff");
